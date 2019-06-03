539a30b506d6b53278a773b97921aa99d9e78db2
Anton Romanov <antonromanov@list.ru>
2019-06-03 18:33:46 +0300

    Научился рест-темплейтом отправлять POST на isimple

--- src/main/java/club/apibank/moedelo/controllers/SerialCheck.java
+++ src/main/java/club/apibank/moedelo/controllers/SerialCheck.java
@@ -1,21 +1,27 @@
 package club.apibank.moedelo.controllers;
 
 import okhttp3.*;
+import org.springframework.http.*;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/rest")
 public class SerialCheck {
 
 	@GetMapping("/test")
-	public String newRequest() throws IOException {
+	public String newRequest() throws IOException, URISyntaxException {
 
 //		OkHttpClient client = new OkHttpClient();
 //
@@ -43,23 +49,61 @@ public class SerialCheck {
 //		Response response1 = client.newCall(request).execute();
 
 
-		try {
+		/*try {
 			HttpClient httpClient = HttpClient.newHttpClient();
 
 			HttpRequest httpRequest = HttpRequest.
-					newBuilder().uri(new URI("http://www.codenuclear.com")).GET().build();
+					newBuilder()
+					.uri(new URI("http://localhost:1081/rest/stateful/corp/login"))
+					.header("authorization", "Basic dHNhcmV2OnRzYXJldg==")
+					.header("host", "bank.test.apibank.club")
+					.POST(HttpRequest.BodyPublishers.noBody())
+					.build();
 
 			HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 			System.out.println(httpResponse.statusCode());
 
 		} catch (Exception e) {
 			System.out.println("message " + e);
-		}
+		}*/
 
 
+	//	RestTemplate restTemplate = new RestTemplate();
+
+		final String baseUrl = "http://localhost:1081/rest/stateful/corp/login";
+		URI uri = new URI(baseUrl);
+		//HttpEntity<String> request = new HttpEntity<>((""));
+
+		//Employee employee = new Employee(null, "Adam", "Gilly", "test@email.com");
+
+		//ResponseEntity<String> result = restTemplate.postForEntity(uri, null, String.class);
 
 
+		// HttpHeaders
+		HttpHeaders headers = new HttpHeaders();
 
-		return "OK";
+		headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+		// Request to return JSON format
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("authorization", "Basic dHNhcmV2OnRzYXJldg==");
+		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+		headers.setHost(new InetSocketAddress("bank.test.apibank.club", 8080));
+		//headers.set("Host", "bank.test.apibank.club");
+
+		// HttpEntity<String>: To get result as String.
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+		// RestTemplate
+		RestTemplate restTemplate = new RestTemplate();
+
+		// Send request with GET method, and Headers.
+		ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+
+		String result = response.getBody();
+
+		System.out.println(result);
+
+
+		return result;
 	}
 }
